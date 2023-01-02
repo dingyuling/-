b06be491dda5c9bf9f12e89ff2b323b04bec5a35
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-01-02 23:49:59 +0800

    十色

---
+++ 十色/begin.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+
+void begin() 
+{
+	printf("\n");
+	printf("			    十色\n");
+	printf("\n");
+	printf("\n");
+	printf("		一片荒芜之间	你是唯一种繁茂\n");
+	Sleep(500);
+	printf("\n");
+	printf("		广袤草原之中	你是第二种美景\n");
+	Sleep(500);
+	printf("\n");
+	printf("		月色雪色之间	你是第三种绝色\n");
+	Sleep(500);
+	printf("\n");
+	printf("		日月星辉之间	你是第四种难得\n");
+	Sleep(500);
+	printf("\n");
+	printf("		风花雪月之间	你是第五种不舍\n");
+	Sleep(500);
+	printf("\n");
+	printf("		乐艺五道之中	你是第六种琴瑟\n");
+	Sleep(500);
+	printf("\n");
+	printf("		浮生六欲之间	你是第七种留恋\n");
+	Sleep(500);
+	printf("\n");
+	printf("		阴阳五行之间	你是第八种超然\n");
+	Sleep(500);
+	printf("\n");
+	printf("		琴棋八雅之间	你是第九种温柔\n");
+	Sleep(500);
+	printf("\n");
+	printf("		太阳星系之外	你是第十种向往\n");
+	Sleep(500);
+	printf("\n");
+
+}
+
+int main(void)
+{
+	begin();
+
+	printf("\t\t");
+
+	system("pause");
+
+	return 0;
+}
