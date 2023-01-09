7b18f68307899b8cf1096779491c987a00ee972a
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-01-09 21:34:33 +0800

    程序调用

---
+++ 程序调用/dy.c
@@ -0,0 +1,17 @@
+#include"dy.h"
+
+int main(void)
+{
+	system("dir");
+
+	char* c = "C:\\Users\\86152\\Desktop\\头像专用.jpg";
+	system(c);
+
+	char* x = "D:\\Yingyong\\Yingyong\\Shejiaoruanjian\\QQ\\Bin\\QQScLauncher.exe";
+	system(x);
+
+	printf("输入：“C:\\Users\\86152\\Desktop\\头像专用.jpg”\n");
+	system("cmd");
+
+	return 0;
+}
---
+++ 程序调用/dy.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
